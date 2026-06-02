8eca1341580bbbffffa661e80db99013cbda6dad
jamesstocktonj1 <james.stocktonj@gmail.com>
2026-06-02 03:26:27 +0100

    feat: add basic worlds encoder

--- encoder.go
+++ encoder.go
@@ -2,7 +2,6 @@ package wit
 
 import (
 	"bufio"
-	"fmt"
 	"io"
 )
 
@@ -18,6 +17,8 @@ func NewEncoder(w io.Writer) *Encoder {
 
 func (e *Encoder) Encode(w Wit) error {
 	e.encodePackage(w.Package)
+	e.w.WriteRune('\n')
+	e.encodeWorlds(w.Worlds)
 	return e.flush()
 }
 
@@ -29,10 +30,42 @@ func (e *Encoder) flush() error {
 }
 
 func (e *Encoder) encodePackage(p Package) {
-	e.w.WriteString(fmt.Sprintf("package %s:%s", p.Namespace, p.Package))
+	e.w.WriteString("package ")
+	e.w.WriteString(p.Namespace)
+	e.w.WriteRune(':')
+	e.w.WriteString(p.Package)
 	if p.Version != "" {
 		e.w.WriteRune('@')
 		e.w.WriteString(p.Version)
 	}
 	e.w.WriteRune(';')
+}
+
+func (e *Encoder) encodeWorlds(w []World) {
+	for i, n := range w {
+		e.encodeWorld(n)
+		if i != len(w)-1 {
+			e.w.WriteRune('\n')
+		}
+	}
+}
+
+func (e *Encoder) encodeWorld(w World) {
+	e.w.WriteString("world ")
+	e.w.WriteString(w.Name)
+	e.w.WriteString(" {")
+	for _, imp := range w.Imports {
+		e.w.WriteString("\n  import ")
+		e.w.WriteString(imp)
+		e.w.WriteRune(';')
+	}
+	for _, exp := range w.Exports {
+		e.w.WriteString("\n  export ")
+		e.w.WriteString(exp)
+		e.w.WriteRune(';')
+	}
+	if len(w.Imports)+len(w.Exports) > 0 {
+		e.w.WriteRune('\n')
+	}
+	e.w.WriteRune('}')
 }
