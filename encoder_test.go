bf0c8dcf36d308d24c6ef406852a6688a0bfaaf3
jamesstocktonj1 <james.stocktonj@gmail.com>
2026-06-19 21:24:33 +0100

    feat: add unit testing to encoder

--- encoder_test.go
+++ encoder_test.go
@@ -1,6 +1,7 @@
 package wit
 
 import (
+	"bytes"
 	"io"
 	"testing"
 
@@ -10,4 +11,90 @@ import (
 func TestNewEncoder(t *testing.T) {
 	enc := NewEncoder(io.Discard)
 	assert.NotNil(t, enc)
+}
+
+func TestEncoder(t *testing.T) {
+	t.Run("basic write", func(t *testing.T) {
+		buf := bytes.NewBuffer(nil)
+
+		enc := NewEncoder(buf)
+		enc.writeString("foo")
+
+		err := enc.flush()
+		assert.NoError(t, err)
+
+		assert.Equal(t, "foo", buf.String())
+	})
+
+	t.Run("write with indent - no block", func(t *testing.T) {
+		buf := bytes.NewBuffer(nil)
+
+		enc := NewEncoder(buf)
+		enc.writeIndent()
+		enc.writeString("foo")
+
+		err := enc.flush()
+		assert.NoError(t, err)
+
+		assert.Equal(t, "foo", buf.String())
+	})
+
+	t.Run("write with indent - with block", func(t *testing.T) {
+		buf := bytes.NewBuffer(nil)
+
+		enc := NewEncoder(buf)
+		enc.openBlock()
+		enc.writeIndent()
+		enc.writeString("foo")
+		enc.closeBlock()
+
+		err := enc.flush()
+		assert.NoError(t, err)
+
+		assert.Equal(t, "  foo", buf.String())
+	})
+
+	t.Run("write with return", func(t *testing.T) {
+		buf := bytes.NewBuffer(nil)
+
+		enc := NewEncoder(buf)
+		enc.writeString("foo")
+		enc.writeReturn()
+
+		err := enc.flush()
+		assert.NoError(t, err)
+
+		assert.Equal(t, "foo\n", buf.String())
+	})
+
+	t.Run("write with block", func(t *testing.T) {
+		buf := bytes.NewBuffer(nil)
+
+		enc := NewEncoder(buf)
+		enc.writeString("record foo {")
+		enc.writeReturn()
+
+		enc.openBlock()
+
+		enc.writeIndent()
+		enc.writeString("bar: str,")
+		enc.writeReturn()
+
+		enc.writeIndent()
+		enc.writeString("bat: s8")
+		enc.writeReturn()
+
+		enc.closeBlock()
+
+		enc.writeString("}")
+
+		err := enc.flush()
+		assert.NoError(t, err)
+
+		exp := "record foo {\n"
+		exp += "  bar: str,\n"
+		exp += "  bat: s8\n"
+		exp += "}"
+		assert.Equal(t, exp, buf.String())
+	})
 }
