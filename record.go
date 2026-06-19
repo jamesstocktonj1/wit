ae27cac2a8ccdce8c73e390694b308b78a566fa8
jamesstocktonj1 <james.stocktonj@gmail.com>
2026-06-19 22:53:26 +0100

    fix: add missing comma

--- record.go
+++ record.go
@@ -14,11 +14,12 @@ func (e *Encoder) encodeRecord(r Record) {
 	e.writeString("record " + r.Name + " {")
 	e.writeReturn()
 	e.openBlock()
-	for _, f := range r.Fields {
+	for i, f := range r.Fields {
 		e.writeIndent()
-		e.writeString(f.Name)
-		e.writeString(": ")
-		e.encodeType(f.Kind)
+		e.encodeField(f)
+		if i < len(r.Fields)-1 {
+			e.writeString(",")
+		}
 		e.writeReturn()
 	}
 	e.closeBlock()
